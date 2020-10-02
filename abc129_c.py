0fff096a71f44dd78c774253642cfadf8788591c
pc <you@example.com>
2020-10-02 23:52:36 +0900

    abc129_cにコメント追記

--- abc129_c.py
+++ abc129_c.py
@@ -1,4 +1,5 @@
 # abc129_c
+# 動的計画法
 
 n,m = map(int,input().split())
 
@@ -12,6 +13,8 @@ dp = [0 for _ in range(n+1)]
 dp[0] = 1
 
 for i in range(1, n + 1):
+    # 制約により、lは単調増加な数列なので、必ず先頭からヒットする
+    # ヒットするたびに先頭をpopすることで検索を省く
     if l and i == l[0]:
         l.pop(0)
         continue
@@ -19,6 +22,7 @@ for i in range(1, n + 1):
         dp[i] = dp[i-1]
     else:
         dp[i] = max(dp[i], dp[i-1] + dp[i-2]) % 1000000007
+        # 2段連続0なら、その先はずっと0
         if(dp[i] == 0 and dp[i-1] == 0):
             break
 print(dp[n])
